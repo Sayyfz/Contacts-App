31e702a6351dc3a5bfe72c509483b898d444911f
Sayyfz <seif_fire2@hotmail.com>
2022-08-19 13:23:23 +0200

    start

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import {useState ,useEffect} from 'react';
+import "../css/App.css";
+import ListContacts from "./ListContacts"
+import * as ContactsAPI from "../utils/ContactsAPI"
+
+const App = () => {
+  
+  const removeContact = (contact) => {
+
+    ContactsAPI.remove(contact)
+    setContacts(contacts.filter((c) => c.id !== contact.id))
+    // contacts = contacts.filter((c) => c.id !== contact.id)
+  }
+
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const response = await ContactsAPI.getAll()
+      setContacts(response)
+    }
+
+    getContacts()
+  }, [])  
+
+
+ 
+
+  return (
+    <div>
+      <ListContacts contacts = {contacts} onDeleteContact = {removeContact}/>
+    </div>
+  )
+
+  
+}
+
+export default App
